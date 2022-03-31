bdfa126e53df37af07f3b2033c31e0410a9efe08
Jungmic <102244947+Jungmic@users.noreply.github.com>
2022-03-31 20:23:11 +0900

    search

--- products/views.py
+++ products/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 def search(request):
 
     return render(request, 'search.html')
-    
+
+def order_by_rate(request):
+    context = {
+        'range' : range(25),
+    }
+    return render(request, 'order_by_rate.html', context)
+
+def order_by_view(request):
+    context = {
+        'range' : range(25),
+    }
+    return render(request, 'order_by_view.html', context)
+
+def search_error(request):
+
+    return render(request, 'search_error.html')
