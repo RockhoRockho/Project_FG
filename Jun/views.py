41c09c9da7321a46cf41f050dd0c23073f74b63e
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-03-31 14:40:22 +0900

    준영 중간저장 3

--- Jun/views.py
+++ Jun/views.py
@@ -3,12 +3,25 @@ from django.shortcuts import render
 def search_best(request):
     context = {
         'range' : range(25),
+        'product_id' : '',
+        'product_name' : '',
+        'product_price' : '',
+        'product_rating' : '', # review 평점 평균으로 계산
+        'product_delivery': '',
+        'seller_name': '',
     }
     return render(request, 'search_best.html', context)
 
 def search_category(request):
     context = {
         'range' : range(25),
+        'product_id' : '',
+        'product_name' : '',
+        'product_price' : '',
+        'product_rating' : '', # review 평점 평균으로 계산
+        'product_delivery': '',
+        'seller_name': '',
+        'product_category': '',
     }
     return render(request, 'search_category.html', context)
 
