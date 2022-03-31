78525e82493c59f7ef0c5e7c58ce497f9214ecbd
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-03-31 10:18:39 +0900

    준영 2

--- Jun/urls.py
+++ Jun/urls.py
@@ -4,4 +4,5 @@ from Jun import views
 urlpatterns = [
     path('product_best/', views.search_best, name='search_best'),
     path('product_category/', views.search_category, name='search_category'),
+    path('cart/', views.cart, name='cart'),
 ]
--- Jun/views.py
+++ Jun/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 def search_best(request):
     context = {
-        'number' : 25,
+        'range' : range(25),
     }
     return render(request, 'search_best.html', context)
+
+def search_category(request):
+    context = {
+        'range' : range(25),
+    }
+    return render(request, 'search_category.html', context)
+
+def cart(request):
+    context = {
+        
+    }
+    return render(request, 'cart.html', context)
