903a86a83572e1fbc1652a087de350c450786aa2
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-03-31 15:40:53 +0900

    로그인
    로그인

--- wonjun/views.py
+++ wonjun/views.py
@@ -1,3 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    context={
+
+    }
+    return render(request, 'login1.html', context)
