3e730fdb7fe5f4e5c3e114226eb61aecb8874908
Jungmic <102244947+Jungmic@users.noreply.github.com>
2022-04-01 09:09:27 +0900

    작업

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('order_by_rate/', views.order_by_rate, name='order_by_rate'),
+    path('order_by_view/', views.order_by_view, name="order_by_view"),
+    path('search_error/', views.search_error, name='search_error'),
+    path('review/', views.review, name='review'),
 ]
