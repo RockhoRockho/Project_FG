241e8e70bda9dc0d2c7385be6a8ca3af04597dcd
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-04-07 14:55:48 +0900

    준영 중간 커밋

--- static/Jun/js/sticky.js
+++ static/Jun/js/sticky.js
@@ -3,10 +3,10 @@ var stickyOrder = $('.order_sticky').offset();
 $(window).scroll(function() {
   if ($(document).scrollTop() > stickyOrder.top) {
     $('.order_sticky').addClass('is_fixed');
-    $('.order_agree').css('margin-top', '314px')
+    $('.order_agree').css('margin-top', '360px')
   }
   else {
     $('.order_sticky').removeClass('is_fixed')
-    $('.order_agree').css('margin-top', '12px')
+    $('.order_agree').css('margin-top', '60px')
   }
 })
