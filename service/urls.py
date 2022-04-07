a6887ac83213e2ad012198e119f11d856e61d94d
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-04-07 18:03:51 +0900

    url 수정

--- service/urls.py
+++ service/urls.py
@@ -3,7 +3,7 @@ from service import views
 
 urlpatterns = [
     path('<int:member_pk>/', views.question_main, name="question_main"),
-    path('write/<int:member_pk>/', views.question, name="question_write"),
+    path('write/<int:member_pk>/', views.question_write, name="question_write"),
     path('Ok/<int:member_pk>/', views.questionOK, name="questionOK"),
     path('detail/<int:member_pk>/', views.question_detail, name="question_detail"),
     path('update/<int:member_pk>/', views.question_update, name="question_update"),
