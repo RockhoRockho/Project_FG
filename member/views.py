63e25f33076b98b3ac9f78ce0cd262c38d6afcb6
Peter-Duwon-Kim <bwk2037@gmail.com>
2022-04-10 20:09:12 +0900

    회원 + base
    member views랑 base에 로그인 링크 수정

--- member/views.py
+++ member/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from member.models import Member
 
 def member_login(request):
     context={
@@ -7,7 +8,19 @@ def member_login(request):
     return render(request, 'member_login.html', context)
     
 def member_join(request):
-    return render(request, "member_join.html")
+    if request.method =='GET':
+        return render(request, 'member_join.html')
+    elif request.method== 'POST':
+        username = request.POST['username']
+        pw = request.POST['pw']
+        name = request.POST['name']
+        birth = request.POST['pw']
+        email = request.POST['email']
+        phoneNum = request.POST['phoneNum']
+    
+        memberT = Member(username = username, pw= pw, name = name, birth = birth, email = email, phoneNum = phoneNum)
+        memberT.save()
+        return render(request, 'member.html', {'pk': memberT.pk})
 
 def member_terms(request):
     return render(request, "member_terms.html")
@@ -17,4 +30,3 @@ def member_info(request):
 
 def member_check(request):
     return render(request, "member_check.html")
-
