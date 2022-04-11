3947e70ece8d83181659bd8b1755e0be9ebffbfd
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-04-11 10:40:47 +0900

    url 2차 수정

--- order/urls.py
+++ order/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from order import views
 
 urlpatterns = [
-    path('list/', views.order_list, name='order_list.'),
+    path('list/', views.order_list, name='order_list'),
     path('detail/', views.order_detail, name='order_detail'),
     path('cart/', views.order_cart, name='order_cart'),
     path('purchase/', views.order_purchase, name='order_purchase'),
