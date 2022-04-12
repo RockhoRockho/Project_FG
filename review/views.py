8c4f3e2dbeb10acaeaa09e9abbdae5893a3a1dd5
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-04-12 22:56:48 +0900

    리뷰쪽 건듬

--- review/views.py
+++ review/views.py
@@ -1,4 +1,28 @@
+from urllib.request import Request
 from django.shortcuts import render
+from review.models import Review
+from member.models import Member
+from django.http import Http404
 
 def review(request):
-    return render(request, 'review.html')
+    all_review = Review.objects.all().order_by('-id')
+    all_count = Review.objects.all().count()
+
+    context = {
+        'review' : all_review,
+        'count' : all_count
+    }
+    return render(request, 'review.html', context)
+
+def review_write(request):
+    return render(request, 'review_update.html')
+
+def review_detail(request):
+    return render(request, 'review_update.html')
+
+def review_update(request):
+    return render(request, 'review_update.html')
+
+    
+def review_delete(request):
+    return render(request, 'review_update.html')
