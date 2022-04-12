8bd471dd360cab693f6d8db51d39db28158e9b87
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-04-12 10:35:20 +0900

    고객센터

--- service/urls.py
+++ service/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('write/', views.question_write, name="question_write"),
     path('Ok/', views.questionOK, name="questionOK"),
     path('detail/<int:id>/', views.question_detail, name="question_detail"),
-    path('update/', views.question_update, name="question_update"),
+    path('update/<int:id>', views.question_update, name="question_update"),
     path('delete/', views.question_delete, name="question_delete"),
     path('answer/', views.question_answer, name="question_answer"),
 ]
