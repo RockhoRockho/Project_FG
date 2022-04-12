837bcbea8032ab25df9918a9605ef1a08fa7b814
Peter-Duwon-Kim <bwk2037@gmail.com>
2022-04-12 15:54:19 +0900

    찜 완료

--- pick/urls.py
+++ pick/urls.py
@@ -3,4 +3,6 @@ from pick import views
 
 urlpatterns = [
     path('', views.pick, name='pick'),
+    path('update/<int:pk>/', views.pick_update, name='pick_update'),
+    path('delete/', views.pick_delete, name='pick_delete'),
 ]
--- pick/views.py
+++ pick/views.py
@@ -1,31 +1,46 @@
 from django.shortcuts import render
 from pick.models import Pick
-from member.models import Member
+from product.models import Product
 
 def pick(request):
-    x = ['x'] #무시
-    all_pick = Pick.objects.all().order_by('id')
-    for i in all_pick: #무시
-        x.append(i) #무시
+    ppick = []
+    pprod = []
+    all_pick = list(Pick.objects.all().order_by('id'))
+    for i in all_pick:
+        if i.member_id == request.session.get('user'):
+            prod = Product.objects.get(pk=i.product_id)
+            pprod.append(prod)
+            ppick.append(i)
     context = {
-        'picks' : all_pick,
-        'ppp' : x, #무시
+        'picks' : ppick,
+        'prods' : pprod,
+        
     }
     return render(request, 'pick.html', context)
 
-#def pick_update(request):
-#    if request.method == 'GET':
-#        pk = request.POST['data-bs-whatever']
-#        pickT = Pick.objects.get(pk=pk)
-#        return render(request, 'pick.html', {'pick':pickT})
-#    elif request.method== 'POST':
-#        
-#        reason = request.POST['reason']
-#        comment = request.POST['comment']
-#
-#        pickT = Pick.objects.get(pk=pk)
-#
-#        pickT.reason = reason
-#        pickT.comment = comment
-#        pickT.save()
-#        return render(request, 'pickOk.html', {'pk': pickT.pk})
+def pick_update(request, pk):
+    if request.method == 'GET':
+        pickT = Pick.objects.get(pk=1)
+        context = {
+            'pick':pickT,
+        }
+
+        return render(request, 'pick_update.html', context)
+    elif request.method== 'POST':
+        
+        reason = request.POST['reason']
+        comment = request.POST['comment']
+
+        pickT = Pick.objects.get(pk=pk)
+        pickT.reason = reason
+        pickT.comment = comment
+        pickT.save()
+        return render(request, 'pick_updateOk.html', {'pk': pickT.pk})
+
+def pick_delete(request):
+    if request.method =='POST':
+        id = request.POST['id']
+        pickT = Pick.objects.get(id=id)
+        pickT.delete()
+
+        return render(request, 'pick_deleteOk.html')
