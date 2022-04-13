7597906b63bc0d737d0a9e999991eb104a0b53b3
Peter-Duwon-Kim <bwk2037@gmail.com>
2022-04-13 14:58:29 +0900

    오류 수정

--- member/context_processors.py
+++ member/context_processors.py
@@ -2,10 +2,12 @@ from member.models import Member
 
 def flag_processor(request):
     flag = 1
+    member = 0
     if request.session.get('user') :
         flag = 1
+        member = Member.objects.get(member_id = request.session.get('user'))
     else:
         flag = 0    
+    
 
-    member = Member.objects.get(member_id = request.session.get('user'))
     return {'flag': flag, 'member':member}
