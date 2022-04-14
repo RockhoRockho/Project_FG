578fc7fb151820eb51de7ce9d3da62b114c7632a
Leyhara <vocalold@naver.com>
2022-04-15 01:40:35 +0900

    admin

--- APPS/PICK/admin.py
+++ APPS/PICK/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pick
+
+class PickAdmin(admin.ModelAdmin):
+    list_display = ('member', 'product', 'comment')
+    icon_name = 'favorite'
+    search_fields = ['member', 'product']
+
+admin.site.register(Pick, PickAdmin)
